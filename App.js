31de98f00d0660e586b7f0b976e32aa414004db8
lin <lin7cb@outlook.com>
2024-03-24 13:22:02 +0000

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,15 @@
 import Start from './components/Start';
 import Chat from './components/Chat';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import {
+    getFirestore,
+    disableNetwork,
+    enableNetwork,
+} from 'firebase/firestore';
 import { LogBox, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { useEffect } from 'react';
 
 LogBox.ignoreLogs(['AsyncStorage has been extracted from']);
 
@@ -30,12 +37,28 @@ const App = () => {
     // Initialize Cloud Firestore and get a reference to the service
     const db = getFirestore(app);
 
+    const connectionStatus = useNetInfo();
+    useEffect(() => {
+        if (connectionStatus.isConnected === false) {
+            Alert.alert('Connection Lost!');
+            disableNetwork(db);
+        } else if (connectionStatus.isConnected === true) {
+            enableNetwork(db);
+        }
+    }, [connectionStatus.isConnected]);
+
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Start'>
                 <Stack.Screen name='Start' component={Start} />
                 <Stack.Screen name='Chat'>
-                    {(props) => <Chat db={db} {...props} />}
+                    {(props) => (
+                        <Chat
+                            db={db}
+                            isConnected={connectionStatus.isConnected}
+                            {...props}
+                        />
+                    )}
                 </Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
