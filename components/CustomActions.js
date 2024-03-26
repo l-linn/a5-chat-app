68a5e2f335fc89a18b5b6270f6bbee5ef87954bb
lin <lin7cb@outlook.com>
2024-03-26 18:30:03 +0000

    Update CustomActions.js

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -2,9 +2,17 @@ import { TouchableOpacity, View, Text, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
 import { useActionSheet } from '@expo/react-native-action-sheet';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
-const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
+const CustomActions = ({
+    wrapperStyle,
+    iconTextStyle,
+    storage,
+    onSend,
+    userID,
+}) => {
     const actionSheet = useActionSheet();
+
     const onActionPress = () => {
         const options = [
             'Choose From Library',
@@ -33,6 +41,28 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
             }
         );
     };
+
+    const pickImage = async () => {
+        let permissions =
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (permissions?.granted) {
+            let result = await ImagePicker.launchImageLibraryAsync();
+            if (!result.canceled)
+                await uploadAndSendImage(result.assets[0].uri);
+            else Alert.alert("Permissions haven't been granted.");
+        }
+    };
+
+    const takePhoto = async () => {
+        let permissions = await ImagePicker.requestCameraPermissionsAsync();
+        if (permissions?.granted) {
+            let result = await ImagePicker.launchCameraAsync();
+            if (!result.canceled)
+                await uploadAndSendImage(result.assets[0].uri);
+            else Alert.alert("Permissions haven't been granted.");
+        }
+    };
+
     const getLocation = async () => {
         let permissions = await Location.requestForegroundPermissionsAsync();
         if (permissions?.granted) {
@@ -46,6 +76,24 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
                 });
             } else Alert.alert('Error occurred while fetching location');
         } else Alert.alert("Permissions haven't been granted.");
+    };
+
+    const generateReference = (uri) => {
+        // this will get the file name from the uri
+        const imageName = uri.split('/')[uri.split('/').length - 1];
+        const timeStamp = new Date().getTime();
+        return `${userID}-${timeStamp}-${imageName}`;
+    };
+
+    const uploadAndSendImage = async (imageURI) => {
+        const uniqueRefString = generateReference(imageURI);
+        const newUploadRef = ref(storage, uniqueRefString);
+        const response = await fetch(imageURI);
+        const blob = await response.blob();
+        uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+            const imageURL = await getDownloadURL(snapshot.ref);
+            onSend({ image: imageURL });
+        });
     };
 
     return (
@@ -73,7 +121,7 @@ const styles = StyleSheet.create({
     iconText: {
         color: '#b2b2b2',
         fontWeight: 'bold',
-        fontSize: 10,
+        fontSize: 16,
         backgroundColor: 'transparent',
         textAlign: 'center',
     },
